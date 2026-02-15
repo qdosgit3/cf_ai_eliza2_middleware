42751b922db5651a9ddb8921a4f14221525e5184
qdosgit3 <228497977+qdosgit3@users.noreply.github.com>
2026-02-15 19:48:48 +0000

    Missed the boat on CORS and it was an unexpected invention

--- hello-ai/src/index.ts
+++ hello-ai/src/index.ts
@@ -12,20 +12,23 @@
  */
 
 export interface Env {
-  // If you set another name in the Wrangler config file as the value for 'binding',
-  // replace "AI" with the variable name you defined.
-  AI: Ai;
+    // If you set another name in the Wrangler config file as the value for 'binding',
+    // replace "AI" with the variable name you defined.
+    AI: Ai;
 }
 
 export default {
     async fetch(request, env): Promise<Response> {
 
+	const { url } = request;
+
+	console.log(url);
 
 	//  https://developers.cloudflare.com/workers/examples/read-post/
 	async function readRequestBody(request: Request) {
 	    const contentType = request.headers.get("content-type");
 	    if (contentType.includes("application/json")) {
-		return (await request);
+		return (await request.json());
 	    } else if (contentType.includes("application/text")) {
 		return request.text();
 	    } else if (contentType.includes("text/html")) {
@@ -36,7 +39,8 @@ export default {
 		for (const entry of formData.entries()) {
 		    body[entry[0]] = entry[1];
 		}
-		return body;
+		return JSON.stringify(body);
+
 	    } else {
 		// Perhaps some other type of data was submitted in the form
 		// like an image, or some other binary data.
@@ -44,32 +48,70 @@ export default {
 	    }
 	}
 	
-	const { url } = request;
 	if (url.includes("form")) {
 	    return rawHtmlResponse(someForm);
 	}
-	if (request.method === "POST") {
 
-	    //  Working around quirks of readRequestBody().
-	    
+	
+	//  https://developers.cloudflare.com/workers/examples/cors-header-proxy/
+
+	async function handleOptions(request) {
+	    if (
+		request.headers.get("Origin") !== null &&
+		    request.headers.get("Access-Control-Request-Method") !== null &&
+		    request.headers.get("Access-Control-Request-Headers") !== null
+	    ) {
+		// Handle CORS preflight requests.
+		return new Response(null, {
+		    headers: {
+			...corsHeaders,
+			"Access-Control-Allow-Headers": request.headers.get(
+			    "Access-Control-Request-Headers",
+			),
+		    },
+		});
+	    } else {
+		// Handle standard OPTIONS request.
+		return new Response(null, {
+		    headers: {
+			Allow: "GET, HEAD, POST, OPTIONS",
+		    },
+		});
+	    }
+	}
+
+	const url_req = new URL(request.url);
+	
+	if (request.method === "OPTIONS") {
+            // Handle CORS preflight requests
+            return handleOptions(request);
+	} else if (request.method === "POST") {
+
 	    const reqBody = await readRequestBody(request);
 
 	    console.log(reqBody);
-
-	    const prompt_kv_str = Object.keys(reqBody)[0];
-
-	    console.log(prompt_kv_str);
-	    
-	    const prompt_in = JSON.parse(prompt_kv_str)["prompt"];
 	    
 	    
 	    const response = await env.AI.run("@cf/meta/llama-3.1-8b-instruct", {
-		prompt: prompt_in,
+		prompt: reqBody["prompt"],
 	    });
+
+	    console.log(response)
+
+	    
+	    // https://developers.cloudflare.com/workers/examples/cors-header-proxy/
+	    
+	    const corsHeaders = {
+		"Access-Control-Allow-Origin": "*",
+		"Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+		"Access-Control-Max-Age": "86400",
+	    };
+
+	    
 	    return new Response(JSON.stringify(response));
 	}
 
-	return JSON.stringify(response);
-	
-    },
-} satisfies ExportedHandler<Env>;
+	return Response(url_req);
+
+    }
+}satisfies ExportedHandler<Env>;
